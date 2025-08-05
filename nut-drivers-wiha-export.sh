e90a72f3fc08e39d28858b7d68e982e04a1bbddf
Alexander Courtis <alex@courtis.org>
2025-08-05 17:57:37 +1000

    nut-drivers-wiha

---
+++ nut-drivers-wiha-export.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+for c in \
+	"nut-drivers-wiha-1-3-big-end" \
+	"nut-drivers-wiha-4-6-big-end" \
+	"nut-drivers-wiha-7-big-end" \
+	"nut-drivers-wiha-1-3-little-end" \
+	"nut-drivers-wiha-4-6-little-end" \
+	"nut-drivers-wiha-7-little-end"
+do
+	echo "----${c}----"
+	openscad-nightly \
+		pegstr.scad \
+		--backend Manifold \
+		-p pegstr.json \
+		-P "${c}" \
+		-o "${c}.stl"
+done
